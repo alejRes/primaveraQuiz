efe079d5ef8d0157e1d9af412118b0b2e5b0cf25
alejRes <alejrespal@gmail.com>
2021-05-22 16:24:51 +0200

    create function checkAnswer

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let questions = [{
     label: 'Cual es el resultado de multiplicar 4 x 5',
     answers: [
         { label: '20', value: '20' },
-        { label: '15', value: '15'},
+        { label: '15', value: '15' },
         { label: '12', value: '12' },
-        { label: '16', value: '16'},
+        { label: '16', value: '16' },
     ],
     correct: '20'
 },
@@ -13,21 +13,21 @@ let questions = [{
     name: 'Dibujos',
     label: '¿Qué animal es piolin?',
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Un canario', value: 'Un canario' },
+        { label: 'Un gato', value: 'Un gato' },
+        { label: 'Un perro', value: 'Un perro' },
+        { label: 'Un elefante', value: 'Un elefante' },
     ],
     correct: 'Un canario'
 },
 {
     name: 'Literatura',
-    label: '¿Qué animal es piolin?',
+    label: "Who sings the rap song &quot;Secret Wars Part 1&quot;?",
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Un canario', value: 'Un canario' },
+        { label: 'Un gato', value: 'Un gato' },
+        { label: 'Un perro', value: 'Un perro' },
+        { label: 'Un elefante', value: 'Un elefante' },
     ],
     correct: 'Un canario'
 },
@@ -35,10 +35,10 @@ let questions = [{
     name: 'Dibujos',
     label: '¿Qué animal es piolin?',
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Un canario', value: 'Un canario' },
+        { label: 'Un gato', value: 'Un gato' },
+        { label: 'Un perro', value: 'Un perro' },
+        { label: 'Un elefante', value: 'Un elefante' },
     ],
     correct: 'Un canario'
 },
@@ -46,10 +46,10 @@ let questions = [{
     name: 'Dibujos',
     label: '¿Qué animal es piolin?',
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Un canario', value: 'Un canario' },
+        { label: 'Un gato', value: 'Un gato' },
+        { label: 'Un perro', value: 'Un perro' },
+        { label: 'Un elefante', value: 'Un elefante' },
     ],
     correct: 'Un canario'
 },
@@ -57,25 +57,26 @@ let questions = [{
     name: 'Dibujos',
     label: '¿Qué animal es piolin?',
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Un canario', value: 'Un canario' },
+        { label: 'Un gato', value: 'Un gato' },
+        { label: 'Un perro', value: 'Un perro' },
+        { label: 'Un elefante', value: 'Un elefante' },
     ],
     correct: 'Un canario'
 },
 {
     name: 'Dibujos',
-    label: '¿Qué animal es piolin?',
+    label: '¿Con que nombre se conozo al sonido que hacen las gallinas?',
     answers: [
-        { label: 'Un canario' , value: 'Un canario'},
-        { label: 'Un gato', value: 'Un gato'  },
-        { label: 'Un perro', value: 'Un perro'  },
-        { label: 'Un elefante', value: 'Un elefante'  },
+        { label: 'Cacarear', value: 'Cacarear' },
+        { label: 'Rebuznar', value: 'Rebuzanar' },
+        { label: 'Ladrar', value: 'Ladrar' },
+        { label: 'Ulular', value: 'Ulular' },
     ],
-    correct: 'Un canario'
+    correct: 'Cacarear'
 }]
 
+let score = 0;
 // function that messes up the awnser
 const unordenedList = (arrayOrdened) => {
     let arrayUnordened = [];
@@ -92,86 +93,91 @@ const unordenedList = (arrayOrdened) => {
 const printQuestion = (questions, index) => {
     console.log(questions[index].answers)
     let respuetas = unordenedList(questions[index].answers) //save a messy array
-    
+
     let container = document.getElementById('divQuest');
     let quest = document.createElement("h2")
-    let txtquest = document.createTextNode(questions[index].label)
-    quest.appendChild(txtquest)
+    quest.innerHTML=questions[index].label
+    // let txtquest = document.createTextNode(questions[index].label)
+    // quest.appendChild(txtquest)
     container.appendChild(quest)
     respuetas.forEach((element, i) => {
         let input = document.createElement("input")
         input.setAttribute('type', 'radio')
-        input.setAttribute('name', 'awnser')
-        input.setAttribute('id', `awnser${i + 1}`)
+        input.setAttribute('name', 'answer')
+        input.setAttribute('id', `answer${i + 1}`)
         input.setAttribute('value', element.value)
+        input.setAttribute('class', 'answer')
         let label = document.createElement('label')
-        label.setAttribute('for', `awnser${i + 1}`)
-        label.innerText = element.label
+        label.setAttribute('for', `answer${i + 1}`)
+        label.innerHTML = element.label
         container.appendChild(label)
         container.appendChild(input)
     })
     let btn = document.createElement('button')
-    btn.innerText="Siguiente"
-    btn.setAttribute("id","btnNext")
+    btn.innerText = "Siguiente"
+    btn.setAttribute("id", "btnNext")
+    
+    if (index == (questions.length - 1)) {
+        btn.innerText = ""
+        btn.innerText = "Finalizar"
+
+    }
     btn.addEventListener('click', (event) => {
-        
-        /* document.getElementsByName('awnser').forEach(element => {
-            if (element.checked) {
-                console.log(element.value)
-                console.log(event.target.innerText)
-            }
-        });
-        if(index!=(questions.length-1)){
-            index++
-            container.innerHTML=""
-            printQuestion(questions,index)
-        } */
-        changeCheck(questions[index].correct,index,event,container)
+        // let hoy, fecha;
+        if (btn.textContent != "Finalizar") {
+            score = checkAnswer(score,questions[index].correct)
+            changeCheck(/* questions[index].correct, */ index, container)
+        } else {
+            let hoy, fecha;
+            hoy = new Date()
+            fecha = `${hoy.getDate()}-${(hoy.getMonth() + 1)}-${hoy.getFullYear()} ${event.timeStamp}`
+            score = checkAnswer(score,questions[index].correct)
+            // results(score, fecha)
+            // console.log(score)
+            location.assign(`results.html`)
+            
+        }
     })
     container.appendChild(btn)
 }
-// function that change and check the targets
-const changeCheck =(ansTrue, i,e,container)=>{
-    document.getElementsByName('awnser').forEach(element => {
-        if (element.checked) {
-            console.log(element.value)
-            console.log(e.target.innerText)
-        }
-    });
-    if(i!=(questions.length-1)){
-        i++
-        container.innerHTML=""
-        printQuestion(questions,i)
-    }
-    if (true) {
-        
-    }
-
+// funtion that print the result in result.html
+const printresult = () =>{
+    
 }
 
-printQuestion(questions,0)
-
-/* document.getElementById('btnNext').addEventListener('click', (event) => {
-    let hoy = new Date()
-    let fecha = `${hoy.getDate()}-${(hoy.getMonth() + 1)}-${hoy.getFullYear()} ${event.timeStamp}`
-
-    console.log(fecha)
-    // result(fecha,puntuacion);
-    document.getElementsByName('awnser').forEach(element => {
-        if (element.checked) console.log(element.value)
-        
+// function that save the score 
+const checkAnswer = (score, aswTrue) =>{
+    document.getElementsByName('answer').forEach(element => {
+        console.log(element.checked)
+        if (element.checked) {
+            if(element.value == aswTrue){
+                score++
+                let scoreSave = JSON.stringify(score)
+                localStorage.setItem("score", scoreSave)
+            }                
+        }
     });
-}) */
+    return score
+}
 
-// function that messes up the awnser
-/* const unordenedList = (arrayOrdened) => {
-    let arrayUnordened = [];
-    let itera = arrayOrdened.length
-    for (let i = 0; i < itera; i++) {
-        let num = Math.floor(Math.random() * arrayOrdened.length)
-        arrayUnordened.push(arrayOrdened[num])
-        arrayOrdened.splice(num, 1)
-        return arrayUnordened
+// function that change and check the targets
+const changeCheck = (i, container) => {
+   
+    if (i != (questions.length - 1)) {
+        i++
+        container.innerHTML = ""
+        printQuestion(questions, i)
     }
-} */
+}
+console.log(location)
+
+if(location.pathname =="/question.html"){
+    // questionOnLoad()
+    printQuestion(questions, 0)
+}else if(location.pathname == "/results.html"){
+    printresult();
+}
+
+
+
 
