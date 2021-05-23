43cad2fe2ce4849cb6ff258c622e462486eda361
Miku <miguel.plopez1000@gmail.com>
2021-05-23 18:17:17 +0200

    actualizado grafica

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
+let graficDates
+let graficValues
+let fechas
 let keys
 let values
 let horaPuntuacion ={}
-let result=(resultado, date)=>{horaPuntuacion[date]=resultado
+let result=(resultado, date)=>{
+    if(localStorage.getItem('quentinTarantino')==undefined){
+    horaPuntuacion[date]=resultado
     localStorage.setItem(
      "quentinTarantino", JSON.stringify(horaPuntuacion));
      //console.log(horaPuntuacion)
              keys = Object.keys(horaPuntuacion )
-            //console.log(keys)
+            console.log(keys)
              values= Object.values(horaPuntuacion)
-           // console.log(values)
-}
+           console.log(values)
+           filtrar()
+            storagepaintGraphics()
+            pintarUl(graficDates,graficValues)
+}else{
+    horaPuntuacion =JSON.parse( localStorage.getItem('quentinTarantino'))
+    horaPuntuacion[date]=resultado
+    localStorage.setItem(
+    "quentinTarantino", JSON.stringify(horaPuntuacion));
+            keys = Object.keys(horaPuntuacion )
+            console.log(keys)
+             values= Object.values(horaPuntuacion)
+            console.log(values)
+            filtrar()
+            storagepaintGraphics()
+            pintarUl(graficDates,graficValues)
+        }
+        }
+
+// let result=(resultado, date)=>{horaPuntuacion[date]=resultado
+//     localStorage.setItem(
+//      "quentinTarantino", JSON.stringify(horaPuntuacion));
+//      //console.log(horaPuntuacion)
+//              keys = Object.keys(horaPuntuacion )
+//             //console.log(keys)
+//              values= Object.values(horaPuntuacion)
+//            // console.log(values)
+// }
      //console.log(horaPuntuacion)
  
-    // result("9","14-8-2021 158")
-    // result("7","53-8-2021 261")
-    // result("1","73-8-2021 391")
-    // result("4","73-8-2021 369")
-    // result("4","73-8-2021 3924")
+     
+    // result("10","53-8-2021 3856")
     // result("4","73-8-2021 3914")
 //console.log(keys)
-let fechas=keys.map((x)=>x.slice(0,9))
+function filtrar() {
+    
+ fechas=keys.map((x)=>x.slice(0,9))
     console.log(fechas)
+
    
 
 
-    let graficDates=[]
+     graficDates=[]
         let numero = fechas
         let hola2 = numero.length-4//(numero.length-(numero.length-4))
         for(i=hola2;i<numero.length;i++){
@@ -32,12 +63,13 @@ let fechas=keys.map((x)=>x.slice(0,9))
         }
 console.log(graficDates)
 
-let graficValues=[]
+ graficValues=[]
         let numero2 = values
         let hola3 = numero2.length-4//(numero.length-(numero.length-4))
         for(i=hola3;i<numero2.length;i++){
             graficValues.push(numero2[i])
         }
+    }
 console.log(graficValues)
 
  storagepaintGraphics =() => {
@@ -125,7 +157,7 @@ console.log(graficValues)
 
             }pintarResultados(puntos)
  } 
- storagepaintGraphics()
+ 
  
 
 function pintarUl(a,b) {for (let i = 0; i < b.length; i++) {
@@ -137,5 +169,6 @@ function pintarUl(a,b) {for (let i = 0; i < b.length; i++) {
     
     
 }
-pintarUl(graficDates,graficValues)
-//location.assign(`home.html`) 
+
+//location.assign(`home.html`)
+
